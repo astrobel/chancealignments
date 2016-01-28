56a70494bb10f7c47443d60d8fdac71a2a9a3a2c
Isabel C <icol6407@uni.sydney.edu.au>
2016-01-28 14:41:14 +1100

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,12 @@
+# code from http://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
